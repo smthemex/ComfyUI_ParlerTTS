5cec59279952b2ba15038bf4f80861a6b47734c4
smthemex <138738845+smthemex@users.noreply.github.com>
2024-04-16 09:54:03 +0800

    Update parler_tts_node.py
    Streamline code and optimize usage logic
    精简代码，优化使用逻辑

--- parler_tts_node.py
+++ parler_tts_node.py
@@ -26,7 +26,7 @@ class ModelDownload:
                                     {"default": "./models/diffusers"}),
                 "max_workers": ("INT", {"default": 4, "min": 1, "max": 8, "step": 1, "display": "slider"}),
                 "local_dir_use_symlinks": ("BOOLEAN", {"default": True},),
-                "use_hf_mirror": ("BOOLEAN", {"default": True},)
+                "use_hf_mirror": ("BOOLEAN", {"default": False},)
             }
         }
 
@@ -35,39 +35,31 @@ class ModelDownload:
     FUNCTION = "model_download"
     CATEGORY = "Parler_TTS"
 
-    def model_download(self, repo_id, model_local_dir, max_workers, local_dir_use_symlinks, use_hf_mirror):
+    def hf_mirror(self, use_hf_mirror):
         if use_hf_mirror:
-            os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
-
-            from huggingface_hub import snapshot_download
-
-            model_path = f"{model_local_dir}/{repo_id.split('/')[-1]}"  # 本地模型存储的地址
-
-            # 开始下载
-            snapshot_download(
-                repo_id=repo_id,
-                local_dir=model_path,
-                local_dir_use_symlinks=local_dir_use_symlinks,  # 为false时，以本地模型使用文件保存，而非blob形式保存，但是每次使用得重新下载。
-                max_workers=max_workers
-                # token=token,“在hugging face上生成的 自己的access token， 否则模型下载可能会中断”
-                # proxies = {"https": "http://localhost:7890"},  # 可选代理端口
-            )
-            return (model_path,)
+            os.environ['HF_ENDPOINT'] = 'https://huggingface.co'
         else:
-            from huggingface_hub import snapshot_download
+            os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
+        return os.environ['HF_ENDPOINT']
 
-            model_path = f"{model_local_dir}/{repo_id.split('/')[-1]}"  # 本地模型存储的地址
 
-            # 开始下载
-            snapshot_download(
-                repo_id=repo_id,
-                local_dir=model_path,
-                local_dir_use_symlinks=local_dir_use_symlinks,  # 为false时，以本地模型使用文件保存，而非blob形式保存，但是每次使用得重新下载。
-                max_workers=max_workers
-                # token=token,“在hugging face上生成的 自己的access token， 否则模型下载可能会中断”
-                # proxies = {"https": "http://localhost:7890"},  # 可选代理端口
-            )
-            return (model_path,)
+    def model_download(self, repo_id, model_local_dir, max_workers, local_dir_use_symlinks, use_hf_mirror):
+
+        self.hf_mirror(use_hf_mirror)
+        from huggingface_hub import snapshot_download
+
+        model_path = f"{model_local_dir}/{repo_id.split('/')[-1]}"  # 本地模型存储的地址
+
+        # 开始下载
+        snapshot_download(
+            repo_id=repo_id,
+            local_dir=model_path,
+            local_dir_use_symlinks=local_dir_use_symlinks,  # 为false时，以本地模型使用文件保存，而非blob形式保存，但是每次使用得重新下载。
+            max_workers=max_workers
+            # token=token,“在hugging face上生成的 自己的access token， 否则模型下载可能会中断”
+            # proxies = {"https": "http://localhost:7890"},  # 可选代理端口
+        )
+        return (model_path,)
 
 
 class PromptToAudio:
@@ -87,7 +79,7 @@ class PromptToAudio:
                                                       "delivers her words quite expressively, in a very confined "
                                                       "sounding environment with clear audio quality. "
                                                       "She speaks very fast."}),
-                "use_model_offline": ("BOOLEAN", {"default": True},)
+                "get_model_online": ("BOOLEAN", {"default": True},)
             }
         }
 
@@ -96,37 +88,19 @@ class PromptToAudio:
     FUNCTION = "prompt2audio"
     CATEGORY = "Parler_TTS"
 
-    def prompt2audio(self, model_path, prompt, description, use_model_offline):
+    def get_model(self, get_model_online):
+        if get_model_online:
+            os.environ['TRANSFORMERS_OFFLINE'] = "0"
+        else:
+            os.environ['TRANSFORMERS_OFFLINE'] = "1"
+        return os.environ['TRANSFORMERS_OFFLINE']
+
+    def prompt2audio(self, model_path, prompt, description, get_model_online):
         if not model_path:
             raise ValueError("need a model_path")
         else:
-            if use_model_offline:
-                try:
-                    os.environ['TRANSFORMERS_OFFLINE'] = "1"
-
-                    model = ParlerTTSForConditionalGeneration.from_pretrained(model_path).to(device)
-                    tokenizer = AutoTokenizer.from_pretrained(model_path)
-                    input_ids = tokenizer(description, return_tensors="pt").input_ids.to(device)
-                    prompt_input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to(device)
-                    generation = model.generate(input_ids=input_ids, prompt_input_ids=prompt_input_ids)
-                    audio_arr = generation.cpu().numpy().squeeze()
-
-                    file_name = "Audio" + ''.join(random.choice("0123456789") for _ in range(5)) + ".wav"
-                    path = os.path.join(tts_path, "output", file_name)
-                    output_path = os.path.normpath(path)
-                    sf.write(output_path, audio_arr, model.config.sampling_rate)
-
-                    with sf.SoundFile(output_path, mode='r') as f:
-                        duration = f.frames / f.samplerate
-                        audio_len = f'The duration of the audio file is: {duration} seconds.'
-                        audio_file = output_path
-                        audio_info = f"{output_path} \n {audio_len} \n {prompt}"
-                    return audio_file, audio_info
-                except Exception as e:
-                    e = ("Notice:When using use_model_offline, the model path must be the path of existing "
-                         "pre downloaded models.使用use_model_offline时，注意：模型路径必须是已有预下载模型的路径")
-                    print(e)
-            else:
+            self.get_model(get_model_online)
+            try:
                 model = ParlerTTSForConditionalGeneration.from_pretrained(model_path).to(device)
                 tokenizer = AutoTokenizer.from_pretrained(model_path)
                 input_ids = tokenizer(description, return_tensors="pt").input_ids.to(device)
@@ -143,8 +117,12 @@ class PromptToAudio:
                     duration = f.frames / f.samplerate
                     audio_len = f'The duration of the audio file is: {duration} seconds.'
                     audio_file = output_path
-                    audio_info = f"{output_path} \n {audio_len} \n {prompt}"
+                    audio_info = f"{output_path}\n{audio_len}\n{prompt}"
                 return audio_file, audio_info
+            except Exception as e:
+                e = ("Notice: When using \'use_model_offline\', the model path must be the path of existing "
+                     "pre downloaded models.\n 注意： 使用\'use_model_offline\'模式时，模型路径必须是已有预下载模型的路径")
+                print(e)
 
 
 NODE_CLASS_MAPPINGS = {
